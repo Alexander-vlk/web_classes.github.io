8276946cdbd5ea73137e4028315d535ca35e4bd4
Alexander Barmenkov <www.lad282@gmail.com>
2023-09-18 00:01:45 +0300

    added index.js file

---
+++ docs/index.js
@@ -0,0 +1,9 @@
+show = () => {
+    let menu = document.getElementById("menu");
+    menu.style.display = "block";
+    menu.style.top = "12dvh";
+}
+hide = () => {
+    let menu = document.getElementById("menu");
+    menu.style.display = "none";
+}
