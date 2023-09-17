4ff86535ed121a61fd7841303d7b2d417496d187
Alexander Barmenkov <www.lad282@gmail.com>
2023-09-18 00:07:03 +0300

    js code removed

--- docs/index.js
+++
@@ -1,9 +0,0 @@
-show = () => {
-    let menu = document.getElementById("menu");
-    menu.style.display = "block";
-    menu.style.top = "12dvh";
-}
-hide = () => {
-    let menu = document.getElementById("menu");
-    menu.style.display = "none";
-}
